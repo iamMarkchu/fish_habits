903dceeef5043fb74007d762e18d815f3af6eadb
kchu <kchu@i9i8.com>
2019-08-14 18:33:29 +0800

    model抽离

--- models/models.go
+++ models/models.go
@@ -8,38 +8,6 @@ import (
 	"time"
 )
 
-// 类别表
-type Category struct {
-	Id          int       `orm:"auto"`
-	Name        string    `orm:"unique;column(cate_name);description(类别名称)"`
-	ParentId    int       `orm:"column(parent_id);description(父类别id);default(0);"`
-	Description string    `orm:"type(text);description(类别描述)"`
-	Status      uint8     `orm:"default(1);description(用户状态)"`
-	UserId      int       `orm:"default(0);column(user_id);description(创建人)"`
-	Created     time.Time `orm:"auto_now_add;column(created_at);type(datetime)"`
-	Updated     time.Time `orm:"auto_now;column(updated_at);type(datetime)"`
-}
-
-func (c *Category) Store() (int64, error) {
-	var (
-		o        = orm.NewOrm()
-		err      error
-		insertId int64
-	)
-	insertId, err = o.Insert(c)
-	return insertId, err
-}
-
-func (c *Category) GetList() ([]*Category, error) {
-	var (
-		o          = orm.NewOrm()
-		err        error
-		categories []*Category
-	)
-	_, err = o.QueryTable(c).All(&categories)
-	return categories, err
-}
-
 // 习惯表
 type Habit struct {
 	Id      int       `orm:"auto"`
--- services/category.go
+++ services/category.go
@@ -9,19 +9,27 @@ type CategoryService struct {
 }
 
 func (c *CategoryService) Store(f forms.StoreCategoryForm) (int64, error) {
-	category := models.Category{}
+	var (
+		category models.Category
+	)
+
 	category.Name = f.CateName
 	category.ParentId = f.ParentId
 	category.UserId = 1
 	category.Status = 1
+
 	id, err := category.Store()
 	return id, err
 }
 
 func (c *CategoryService) GetList() ([]*models.Category, error) {
-	category := models.Category{}
-	var categories []*models.Category
-	categories, err := category.GetList()
+	var (
+		category   models.Category
+		categories []*models.Category
+		err        error
+	)
+
+	categories, err = category.GetList()
 	return categories, err
 }
 
